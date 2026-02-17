83e15a459c1174789199cdeef49de90fd4390ef7
Muhammad Usman <mu801710@gmail.com>
2026-02-17 07:57:57 +0500

    push

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,3 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000';
+const API_URL = import.meta.env.VITE_API_URL ||
+    (import.meta.env.MODE === 'production' ? '' : 'http://127.0.0.1:5000');
 
 export default API_URL;
